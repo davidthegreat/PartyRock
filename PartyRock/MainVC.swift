c3c690c41a48c6bb847392ff742a690405520425
Macbook Air <macbookair@David-Ramirez.netgear.com>
2017-05-02 21:54:40 -0700

    Initial Commit

---
+++ PartyRock/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by Macbook Air on 5/2/17.
+//  Copyright © 2017 David Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
